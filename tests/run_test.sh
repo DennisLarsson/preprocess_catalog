d9b7fd61cd3537591b336f29c9871a2e6b7c8106
DennisLarsson <dennis.j.larsson@gmail.com>
2024-07-23 10:37:30 +0200

    Skip diff on results.out since it changes when blast updates and remove comments

--- tests/run_test.sh
+++ tests/run_test.sh
@@ -24,7 +24,6 @@ blastn -db nt_euk \
     -outfmt "10 delim=@ qseqid qlen sscinames sblastnames sskingdoms stitle evalue bitscore score length nident qcovs" \
     -out results.out -remote
 
-# Make script testable and create a unittest for it
 ./filter_nonplant_loci.py \
     -b results.out \
     -c catalog_R04_max10snp.fa \
@@ -32,7 +31,4 @@ blastn -db nt_euk \
 
 diff whitelist_R04_max10snp expected_whitelist_R04_max10snp
 diff catalog_R04_max10snp.fa expected_catalog_R04_max10snp.fa
-# diff results.out expected_results.out
-# Write a script that tests for the format of the output, rather than the exact content, which changes with database updates
 diff catalog_R04_max10snp_blasted.fa expected_catalog_R04_max10snp_blasted.fa
-# Modify test files to include much fewer loci to make testing easier to overview and checking for errors
